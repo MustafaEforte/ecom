cf2f887ea0be86c93dc32d765b2ec84a890b328a
maqsoodEforte <maqsood@eforte.net>
2022-11-02 16:11:32 +0500

    navbar-complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ import PersonIcon from "@mui/icons-material/Person";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
 import Image from "next/image";
-// import { logo } from "../public";
+import { logo } from "../public";
 import styled from "styled-components";
 import Link from "next/link";
 import CloseIcon from "@mui/icons-material/Close";
@@ -16,133 +16,131 @@ const Navbar = () => {
   const handleOpen = () => setNavOpen(true);
   const handleClose = () => setNavOpen(false);
 
+  const list = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Shop",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/",
+    },
+    {
+      name: "Latest",
+      path: "/",
+    },
+    {
+      name: "Blog",
+      path: "/",
+    },
+    {
+      name: "Pages",
+      path: "/",
+    },
+  ];
   return (
     <>
-      {navOpen ? (
-        <Grid
-          container
-          mb={10}
-          sx={{
-            width: "100%",
-            zIndex: "9",
-            position: "absolute",
-            backgroundColor: "white",
-            // transitionProperty: "transform",
-            display: {
-              lg: "none",
-              xl: "none"
-            }
-          }}
-        >
+      
+        <Grid container >
           <Grid item xs={12} >
-            <Stack>
-              <CloseIcon onClick={handleClose} />
-              <Link href="/">Home</Link>
-              <Divider />
-              <Link href="/">Shop</Link>
-              <Divider />
-              <Link href="/">About</Link>
-              <Divider />
-              <Link href="/">Latest</Link>
-              <Divider />
-              <Link href="/">Blog</Link>
-              <Divider />
-              <Link href="/">Pages</Link>
-              <Divider />
-            </Stack>
-          </Grid>
-        </Grid>
-      ) : (
-        ""
-      )}
-      <Container
-        maxWidth="xl"
-        disableGutters="true"
-        sx={{ position: "relative" }}
-      >
-        <Grid container>
-          <Grid item xs={12}>
             {/* Web View */}
 
-            <Grid container>
+            <Grid container py={5}>
+              <Grid item xs={4} sm={4} md={2} lg={2} xl={2} sx={{display: "flex",alignItem: "center", justifyContent: "end", width: "100%"}}>
+                  <img src={logo} />
+                
+              </Grid>
+              <Grid item xs={7} sx={{
+                    display: {
+                      xs: "none",
+                      sm: "none",
+                      md: "block",
+                      lg: "block",
+                    },
+                  }}>
+                <Stack
+                  sx={{justifyContent: "center", alignItems: "center"}}
+                  direction="row"
+                  spacing={6}
+                  
+                >
+                  {list.map((item) => {
+                    return <Link href={item.path}>{item.name}</Link>;
+                  })}
+                </Stack>
+              </Grid>
               <Grid
                 item
-                xs={2}
-                sx={{
-                  display: "flex",
-                  justifyContent: "end",
-                  alignItems: "center",
-                }}
+                xs={6}
+                xl={3}
+                lg={3}
+                md={3}
+                sm={5}
+                sx={{alignItems: "center", display: "flex", justifyContent: "center"}}
               >
-                <div>
-                  {/* <img src={logo}/> */}
-                  <Image
-                    src={require("/public/assests/Timed_6_30.png")}
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </div>
-              </Grid>
-              <NavSec
-                item
-                xs={8}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Stack direction="row" spacing={6}>
-                  <Typography>Home</Typography>
-                  <Typography>Shop</Typography>
-                  <Typography>About</Typography>
-                  <Typography>Latest</Typography>
-                  <Typography>Blog</Typography>
-                  <Typography>Pages</Typography>
-                </Stack>
-              </NavSec>
-              <NavSec
-                item
-                xs={1}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Stack direction="row" spacing={4}>
+                <Stack direction="row" spacing={4} >
                   <SearchIcon />
                   <PersonIcon />
                   <ShoppingCartOutlinedIcon />
                 </Stack>
-              </NavSec>
+              </Grid>
               <Grid
                 item
-                xs={10}
-                // sm={4}
-                sx={{
-                  display: "flex",
-                  justifyContent: "end",
-                  alignItems: "center",
-                }}
-              >
+                xs={2}
+                sx={{display: "flex", alignItem: "center", justifyContent: "end"}}
+                >
                 <MenuIcon
                   onClick={handleOpen}
                   fontSize="large"
                   sx={{ display: { md: "none", lg: "none", xl: "none" } }}
                 />
               </Grid>
+              {navOpen ? (
+                <Grid
+                  container
+                  // mb={10}
+                  sx={{
+                    backgroundColor: "grey",
+                    color: "white",
+                    display: {
+                      lg: "none",
+                      xl: "none",
+                    },
+                  }}
+                >
+                  <Grid item xs={12}>
+                    <Stack>
+                      <CloseIcon onClick={handleClose} />
+                      {list.map((item) => {
+                        return (
+                          <>
+                            <Link href={item.path}>{item.name}</Link>
+                            <Divider />
+                          </>
+                        );
+                      })}
+                    </Stack>
+                  </Grid>
+                </Grid>
+              ) : (
+                ""
+              )}
             </Grid>
           </Grid>
         </Grid>
-      </Container>
+     
     </>
   );
 };
 
 export default Navbar;
 
-const NavSec = styled(Grid)`
-  @media screen and (max-width: 899px) {
-    display: none !important;
-  }
-`;
+// const NavSec = styled(Grid)`
+//   @media screen and (max-width: 899px) {
+//     display: none !important;
+//   }
+// `;
